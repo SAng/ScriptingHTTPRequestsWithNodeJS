bd90436227f9a125fcb949adba9ee039fa53f5e8
Spencer Ang <boomerandzapper@gmail.com>
2017-05-08 18:17:05 +0000

    Completed the getAndPrintHTML.js file and verified that it works using node.

---
+++ getAndPrintHTML.js
@@ -0,0 +1,37 @@
+function getAndPrintHTML () {
+
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step2.html'
+  };
+
+  var https = require('https');
+
+  var result = '';
+
+  https.get(requestOptions, function (response) {
+
+    //Set encoding to utf-8
+    response.setEncoding('utf8');
+
+    //Prints each data chunk then starts a new line
+    response.on('data', function (datachunk) {
+      result += datachunk.toString();
+    });
+
+    //Handles Errors
+    response.on('error', function (err) {
+      console.log(err.toString())
+      return err;
+    });
+
+    //Print that the stream has ended
+    response.on('end', function() {
+      console.log(result);
+    });
+
+  });
+
+};
+
+getAndPrintHTML();
