67b0364463885c45e65c1a38dea3b01f60a5fd9c
Spencer Ang <boomerandzapper@gmail.com>
2017-05-08 18:10:39 +0000

    Completed the printingChunks.js file and verified that it works by calling it with node.

---
+++ printinginChunks.js
@@ -0,0 +1,36 @@
+var https = require('https');
+
+function getAndPrintHTMLChunks () {
+
+  //Set Options
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step1.html'
+  };
+
+  https.get(requestOptions, function (response) {
+
+    //Set encoding to utf-8
+    response.setEncoding('utf8');
+
+    //Prints each data chunk then starts a new line
+    response.on('data', function (datachunk) {
+      console.log(datachunk.toString(), '\n');
+    });
+
+    //Handles Errors
+    response.on('error', function (err) {
+      console.log(err.toString())
+      return err;
+    });
+
+    //Print that the stream has ended
+    response.on('end', function() {
+      console.log('Finished Stream');
+    });
+
+  });
+
+};
+
+getAndPrintHTMLChunks();
